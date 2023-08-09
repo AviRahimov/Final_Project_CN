12f10ab8da4fa2eb509bf8a89cc467ae2e7bb01d
Avi Rahimov <rahimovavi3@gmail.com>
2023-08-10 00:35:45 +0300

    Fix the code and add necessary directories.

--- src/main.py
+++ src/main.py
@@ -1,23 +1,35 @@
 import matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
+import os
 matplotlib.use("TkAgg")
 
 # Load the CSV file into a DataFrame
-csv_file = 'src\YOUTUBE_BACKGROUND_TARGET2.csv'
-df = pd.read_csv(csv_file)
+filter_list = ['resources\\WITH_TCP_FILTER_TARGET1.csv', 'resources\\WITH_TCP_FILTER_TARGET2.csv',
+               'resources\\WITH_TCP_FILTER_TARGET3.csv', 'resources\\WITH_TCP_FILTER_TARGET4.csv']
+youtube_list = ['resources\\YOUTUBE_BACKGROUND_TARGET1.csv', 'resources\\YOUTUBE_BACKGROUND_TARGET2.csv',
+                'resources\\YOUTUBE_BACKGROUND_TARGET3.csv', 'resources\\YOUTUBE_BACKGROUND_TARGET4.csv']
 
+# Loop through the CSV files
+for df_name in (filter_list + youtube_list):
+    df = pd.read_csv(df_name)
 
-# Create the bar histogram plot
-plt.figure(figsize=(10, 6))  # Set the figure size (optional)
+    # Create the bar histogram plot
+    plt.figure(figsize=(10, 6))  # Set the figure size (optional)
 
-# Plot the bar histogram with size as bar width and time as bar height
-plt.bar(df['Time'], df['Length'], color='black', width=0.3)
+    # Plot the bar histogram with size as bar width and time as bar height
+    plt.bar(df['Time'], df['Length'], color='black', width=0.3)
 
-# Add labels and title
-plt.xlabel('Time')
-plt.ylabel('Size')
-plt.title('Bar Histogram: Time vs. Size')
+    # Add labels and title
+    plt.xlabel('Time')
+    plt.ylabel('Size')
+    plt.title('Bar Histogram: Time vs. Size')
 
-# Show the plot
-plt.show()
+    # Create a directory named 'res' if it doesn't exist
+    if not os.path.exists('res'):
+        os.makedirs('res')
+    # Save the plot as an image in the 'res' directory
+    plt.savefig(f'res/{df_name[10:-4]}.png')
+
+    # Show the plot (optional)
+    plt.show()
